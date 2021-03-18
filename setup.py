891d76d352c94b810acd63774754198bd6ffbdd5
Philippe Remy <premy.enseirb@gmail.com>
2021-03-18 15:36:33 +0900

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='binance-future',
-    version='1.0',
+    version='2.0',
     description='Binance Futures API',
     author='Philippe Remy',
     license='MIT',
